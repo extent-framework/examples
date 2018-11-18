cf426858f6f4f7ccba68661b1f596bdeb8c98a6f
anshooarora <anshooarora@gmail.com>
2018-11-18 18:28:25 -0500

    Update ScreenshotTests.java

--- extentreports-testng-adapter-example/src/test/java/com/aventstack/extentreports/adapter/testng/tests/ScreenshotTests.java
+++ extentreports-testng-adapter-example/src/test/java/com/aventstack/extentreports/adapter/testng/tests/ScreenshotTests.java
@@ -14,57 +14,56 @@ import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.service.ExtentTestManager;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 
-@Listeners({ExtentITestListenerClassAdapter.class})
+@Listeners({ ExtentITestListenerClassAdapter.class })
 public class ScreenshotTests {
-    
+
+    private static final String IMG_NAME = "screenshot.png";
+
     // A static image stored under classpath
-    private static final String IMG_PATH = "src/test/resources/screenshot.png";
-    
-    // Using the same OUTPUT_PATH as set in extent.properties [extent.reporter.html.out]
+    private static final String IMG_PATH = "src/test/resources/" + IMG_NAME;
+
+    // Using the same OUTPUT_PATH as set in extent.properties
+    // [extent.reporter.html.out]
     private static final String OUTPUT_PATH = "test-output/HtmlReport/";
-    
-    
+
     @Test
     public void passTest() {
         Assert.assertTrue(true);
     }
-    
+
     /**
-     * A screenshot will be attached for failTest 
+     * A screenshot will be attached for failTest
      */
     @Test
     public void failTest() {
         Assert.assertTrue(false);
     }
-    
+
     @AfterMethod
     public synchronized void afterMethod(ITestResult result) throws IOException {
         switch (result.getStatus()) {
-            case ITestResult.FAILURE:
-                ExtentTestManager.getTest(result).fail(
-                        "ITestResult.FAILURE, event afterMethod",
-                        MediaEntityBuilder.createScreenCaptureFromPath(getImage()).build()
-                );
-                break;
-            case ITestResult.SKIP:
-                ExtentTestManager.getTest(result).skip("ITestResult.SKIP, event afterMethod");
-                break;
-            default:
-                break;
+        case ITestResult.FAILURE:
+            ExtentTestManager.getTest(result).fail("ITestResult.FAILURE, event afterMethod",
+                    MediaEntityBuilder.createScreenCaptureFromPath(getImage()).build());
+            break;
+        case ITestResult.SKIP:
+            ExtentTestManager.getTest(result).skip("ITestResult.SKIP, event afterMethod");
+            break;
+        default:
+            break;
         }
     }
-    
+
     /**
-     * !!This code block is just an example only!!
-     * !!Real-world implemention would require capturing a screenshot!!
+     * !!This code block is just an example only!! !!Real-world implemention would
+     * require capturing a screenshot!!
      * 
      * @return Image path
-     * @throws IOException 
+     * @throws IOException
      */
     private String getImage() throws IOException {
-        String sc = "screenshot.png";
-        Files.copy(new File(IMG_PATH).toPath(), new File(OUTPUT_PATH + sc).toPath());
-        return sc;        
+        Files.copy(new File(IMG_PATH).toPath(), new File(OUTPUT_PATH + IMG_NAME).toPath());
+        return IMG_NAME;
     }
-    
+
 }
